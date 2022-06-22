5bcb991eadc01968b8b7abb4381e731dd5a291b4
Delian Zidarov <d.zidarov@gmail.com>
2022-06-22 23:12:09 +0900

    finished front page roughdarft

--- js/scripts.js
+++ js/scripts.js
@@ -66,3 +66,10 @@ sideScrollingDisplays.forEach((display) => {
     });
   }
 });
+//Coloring the ratings stars
+const ratings = document.querySelectorAll(".ratings-display");
+ratings.forEach((rating) => {
+  const percent = (Number(rating.dataset.rating) / 5) * 100;
+  rating.style.background = `linear-gradient(0.25turn, #e8b430 0 ${percent}%, rgba(0, 0, 0, 0) ${percent}% 100%)`;
+  rating.style["background-clip"] = "text";
+});

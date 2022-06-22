43e974ba35fd9b2cf31cf60f0550c128749bdc07
Delian Zidarov <d.zidarov@gmail.com>
2022-06-22 16:38:36 +0900

    made the side scroll size responsively and initialize properly

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// Parallax Element Code
 const parallaxEls = document.querySelectorAll(".parallax");
 window.addEventListener("scroll", function () {
   parallaxEls.forEach((el) => {
@@ -12,4 +13,36 @@ window.addEventListener("scroll", function () {
       el.style.top = scrollDistance + maxHeight * offset < maxHeight ? `${scrollDistance + maxHeight * offset}px` : `${maxHeight}px`;
     }
   });
+});
+//Side Display code
+const sideScrollingDisplays = document.querySelectorAll(".side-scrolling-display");
+sideScrollingDisplays.forEach((display) => {
+  const moveLeft = display.children[0];
+  const moveRight = display.children[1];
+  const contentContainer = display.children[2];
+  let contentContainerWidth = window.innerWidth * 0.8;
+  const contentChildren = Array.from(contentContainer.children);
+  const contentChildWidth = contentChildren[0].clientWidth;
+  let numVisChildren = Math.floor(contentContainerWidth / contentChildWidth);
+  let visibleRange = { left: 0, right: Math.min(numVisChildren, contentChildren.length) };
+
+  contentContainer.style.width = `${contentContainerWidth}px`;
+  renderVisibleChildren();
+  window.addEventListener("resize", () => {
+    renderVisibleChildren();
+  });
+
+  function renderVisibleChildren() {
+    contentContainerWidth = window.innerWidth * 0.8;
+    numVisChildren = Math.floor(contentContainerWidth / (contentChildWidth + 32)) || 1;
+    visibleRange = { left: 0, right: numVisChildren };
+    contentChildren.forEach((el, i) => {
+      if (el.classList.contains("hide-content")) el.classList.remove("hide-content");
+      if (i < visibleRange.left || i >= visibleRange.right) {
+        el.classList.add("hide-content");
+      }
+    });
+  }
+
+  console.log(contentChildren);
 });

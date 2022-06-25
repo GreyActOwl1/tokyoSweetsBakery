c60ee7271a13fcab2bc604eba35337918bdb88c1
Delian Zidarov <d.zidarov@gmail.com>
2022-06-25 19:41:41 +0900

    created a function to generate the html

--- js/shop-scripts.js
+++ js/shop-scripts.js
@@ -48,38 +48,149 @@ const data = {
   },
 };
 const pillLinks = document.querySelectorAll(".pill-link");
-const pillsBread = document.getElementById("pills-bread");
-const pillsPastery = document.getElementById("pills-pastery");
-const pillsSeasonal = document.getElementById("pills-seasonal");
 const selectionContainer = document.getElementById("current-selection");
+const pillsContent = {
+  bread: document.getElementById("pills-bread"),
+  pastery: document.getElementById("pills-pastery"),
+  seasonal: document.getElementById("pills-seasonal"),
+};
+const modalBuyIds = {
+  img: document.getElementById("modal-buy-image"),
+  name: document.getElementById("modal-buy-name"),
+  rating: document.getElementById("modal-buy-rating"),
+  price: document.getElementById("modal-buy-price"),
+  description: document.getElementById("modal-buy-description"),
+  reviews: document.getElementById("modal-buy-reviews"),
+};
 
 pillLinks.forEach((link) => {
   link.addEventListener("click", (e) => {
     selectionContainer.innerHTML = "";
     let currentCat = e.originalTarget.dataset.cat;
 
-    //generate list of items w/ a trigger for a modal
-    //when clicked clear old modal info and generate new
-
+    //Append description to left bar
     let catDescription = document.createElement("div");
     catDescription.innerHTML = `<h3>${data.categories[currentCat].title}</h3><p>${data.categories[currentCat].description}</p>`;
     selectionContainer.appendChild(catDescription);
+    //append products
+    generateProductCards(data.products[currentCat], pillsContent[currentCat]);
+
+    //********* */
+    // const fragment = new DocumentFragment();
+    // data.products[currentCat].forEach((product) => {
+    //   let productCard = document.createElement("a");
+    //   productCard.setAttribute("data-toggle", "modal");
+    //   productCard.setAttribute("data-target", "#buyNow");
+    //   productCard.innerHTML = `<div class="media product-information">
+    //                             <img src=${product.img} class="mr-3" alt=${product.name}>
+    //                             <div class="media-body">
+    //                                 <h5 class="mt-0">${product.name}</h5>
+    //                                 <p><span class="ratings-display" data-rating=${product.rating}><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></span></p>
+    //                                 <p>${product.price}</p>
+    //                             </div>
+    //                         </div>`;
+    //   productCard.addEventListener("click", () => setModal(product));
+    //   fragment.appendChild(productCard);
+    // });
+    // pillsContent[currentCat].innerHTML = "";
+    // pillsContent[currentCat].appendChild(fragment);
+    // colorStars();
+
+    // function setModal(product) {
+    //   modalBuyIds.img.src = product.img;
+    //   modalBuyIds.img.alt = product.name;
+    //   modalBuyIds.name.innerText = product.name;
+    //   modalBuyIds.price.innerText = product.price;
+    //   modalBuyIds.description.innerText = product.description;
+    //   modalBuyIds.rating.setAttribute("data-rating", product.rating);
+    //   modalBuyIds.reviews.innerHTML = "";
+    //   const reviewFragment = new DocumentFragment();
+    //   product.reviews.forEach((review) => {
+    //     const reviewDiv = document.createElement("div");
+    //     reviewDiv.classList.add("media");
+    //     reviewDiv.innerHTML = ` <img src=${review.avatar} class="align-self-center mr-3 avatar-image " alt="GAN-generated-image">
+    //                         <div >
+    //                             <h6>${review.user}</h6>
+    //                             <p>${review.date}</p>
+    //                             <p><span class="ratings-display" data-rating=${review.rating} id="modal-buy-rating"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></span></p>
+    //                         </div>
+    //                         <div class="media-body ">
+    //                             <p>${review.review}</p>
+    //                         </div>`;
+    //     reviewFragment.appendChild(reviewDiv);
+    //   });
+    //   modalBuyIds.reviews.appendChild(reviewFragment);
+    //   colorStars();
+    // }
+    // *****************
   });
 });
 
 //Coloring the ratings stars
-const ratings = document.querySelectorAll(".ratings-display");
-ratings.forEach((rating) => {
-  const percent = (Number(rating.dataset.rating) / 5) * 100;
-  rating.style.background = `linear-gradient(0.25turn, #e8b430 0 ${percent}%, rgba(0, 0, 0, 0.3) ${percent}% 100%)`;
-  rating.style["background-clip"] = "text";
-});
+function colorStars() {
+  const ratings = document.querySelectorAll(".ratings-display");
+  ratings.forEach((rating) => {
+    const percent = (Number(rating.dataset.rating) / 5) * 100;
+    rating.style.background = `linear-gradient(0.25turn, #e8b430 0 ${percent}%, rgba(0, 0, 0, 0.3) ${percent}% 100%)`;
+    rating.style["background-clip"] = "text";
+  });
+}
 // Hiding and showing nav searchbar on shop.html
 window.addEventListener("scroll", () => {
   if (window.scrollY > window.innerHeight * 0.2) {
-    document.querySelector(".scroll-appear").style.display = "block";
+    document.querySelector(".scroll-appear").style.display = "flex";
   }
   if (window.scrollY == 0) {
     document.querySelector(".scroll-appear").style.display = "none";
   }
 });
+colorStars();
+//generate product cards
+function generateProductCards(productArr, catDiv) {
+  const fragment = new DocumentFragment();
+  productArr.forEach((product) => {
+    let productCard = document.createElement("a");
+    productCard.setAttribute("data-toggle", "modal");
+    productCard.setAttribute("data-target", "#buyNow");
+    productCard.innerHTML = `<div class="media product-information">
+                                <img src=${product.img} class="mr-3" alt=${product.name}>
+                                <div class="media-body">
+                                    <h5 class="mt-0">${product.name}</h5>
+                                    <p><span class="ratings-display" data-rating=${product.rating}><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></span></p>
+                                    <p>${product.price}</p> 
+                                </div>
+                            </div>`;
+    productCard.addEventListener("click", () => setModal(product));
+    fragment.appendChild(productCard);
+  });
+  catDiv.innerHTML = "";
+  catDiv.appendChild(fragment);
+  colorStars();
+
+  function setModal(product) {
+    modalBuyIds.img.src = product.img;
+    modalBuyIds.img.alt = product.name;
+    modalBuyIds.name.innerText = product.name;
+    modalBuyIds.price.innerText = product.price;
+    modalBuyIds.description.innerText = product.description;
+    modalBuyIds.rating.setAttribute("data-rating", product.rating);
+    modalBuyIds.reviews.innerHTML = "";
+    const reviewFragment = new DocumentFragment();
+    product.reviews.forEach((review) => {
+      const reviewDiv = document.createElement("div");
+      reviewDiv.classList.add("media");
+      reviewDiv.innerHTML = ` <img src=${review.avatar} class="align-self-center mr-3 avatar-image " alt="GAN-generated-image">
+                            <div >
+                                <h6>${review.user}</h6>
+                                <p>${review.date}</p>
+                                <p><span class="ratings-display" data-rating=${review.rating} id="modal-buy-rating"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></span></p> 
+                            </div>
+                            <div class="media-body ">
+                                <p>${review.review}</p>
+                            </div>`;
+      reviewFragment.appendChild(reviewDiv);
+    });
+    modalBuyIds.reviews.appendChild(reviewFragment);
+    colorStars();
+  }
+}

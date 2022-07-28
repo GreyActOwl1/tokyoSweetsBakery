1e4646d94bffa72a2a0ecbd40c3a6cb8087d9641
Adrian Ea <aeas5454@outlook.com>
2022-07-27 21:28:40 -0400

    add 404 page

--- src/pages/NoPage.js
+++ src/pages/NoPage.js
@@ -1,5 +1,16 @@
+import { Button } from "reactstrap";
+import { Link } from "react-router-dom";
+
 const NoPage = () => {
-    return <div> 404 page </div>
- };
+    return(
+        <div>
+            404 page <br/>
+            <img src="https://media.gettyimages.com/photos/fail-for-preparing-food-picture-id1263422400?s=2048x2048" alt="404 page img" width={"500"} />
+            <br/>            
+            <Button ><Link to="/">Return home</Link></Button>
+    </div>
+
+    ) ;
+  };
   
   export default NoPage;

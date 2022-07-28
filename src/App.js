c3543ac9958de0fdbdff595de38204cfb880c111
Adrian Ea <aeas5454@outlook.com>
2022-07-27 20:04:42 -0400

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,56 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+// import logo from './logo.svg';
+// import { Counter } from './features/counter/Counter';
 import './App.css';
+import "./styles.css";
+import { Routes, Route, } from "react-router-dom";
+import ContactPage from "./pages/ContactPage";
+import HomePage from "./pages/HomePage";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <Header />
+      <Routes>
+        <Route index element={<HomePage/>} />
+        {/* <Route path="About" element={<AboutPage />} /> */}
+        <Route path="Contact" element={<ContactPage />} />
+        <Route path="Shop" element={<ShopPage />} />
+        {/* <Route path="shop/products/:productName" element={<{ProductDetailPage}}/> */}
+        {/* <Route path="Cart" element={<CartPage />} /> */}
+        {/* <Route path="checkout/Shipping" element={<ShippingPage/>} /> */}
+        {/* <Route path="checkout/Payment" element={<PaymentPage />} /> */}
+        <Route path="*" element={<NoPage />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }

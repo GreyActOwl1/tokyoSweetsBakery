90b7b6c4515d8e0b7069f69a900d79d84ca52d6a
Adrian Ea <aeas5454@outlook.com>
2022-07-28 19:24:10 -0400

    change 404 image

--- src/pages/NoPage.js
+++ src/pages/NoPage.js
@@ -5,7 +5,7 @@ const NoPage = () => {
     return(
         <div>
             404 page <br/>
-            <img src="https://media.gettyimages.com/photos/fail-for-preparing-food-picture-id1263422400?s=2048x2048" alt="404 page img" width={"500"} />
+            <img src="https://media.gettyimages.com/photos/fail-for-preparing-food-picture-id1320952160?s=2048x2048" alt="404 page img" width={"500"} />
             <br/>            
             <Button ><Link to="/">Return home</Link></Button>
     </div>
